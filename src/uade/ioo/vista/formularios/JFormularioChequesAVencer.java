b7c64113d1a237c3b16967737a0aa500936f3dc5
aix <cristian.ayciriex@gmail.com>
2017-11-09 23:42:27 -0300

    First Commit Test

--- src/uade/ioo/vista/formularios/JFormularioChequesAVencer.java
+++ src/uade/ioo/vista/formularios/JFormularioChequesAVencer.java
@@ -5,13 +5,13 @@ import javax.swing.JFrame;
 public class JFormularioChequesAVencer extends JFrame {
 
 	/**
-	 * 
+	 * Test
 	 */
 	private static final long serialVersionUID = -3095074904976216955L;
 	
 	public JFormularioChequesAVencer(){
 		this.setSize(540,380);
-		this.setTitle("Cheques de Terceros próximos a vencer");
+		this.setTitle("Cheques de Terceros prï¿½ximos a vencer");
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
